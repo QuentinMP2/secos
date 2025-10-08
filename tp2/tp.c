5a1d1782b9bbb3fdb335d0c1af11f0b71adfc4c2
Quentin <pointeauq@bde.enseeiht.fr>
2025-10-08 17:10:19 +0200

    feat: tp2

--- tp2/tp.c
+++ tp2/tp.c
@@ -1,17 +1,75 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <intr.h>
+
+void print_idt_content(idt_reg_t idtr_ptr)
+{
+	int_desc_t *idt_ptr;
+	idt_ptr = (int_desc_t *)(idtr_ptr.addr);
+	int i = 0;
+	while ((uint32_t)idt_ptr < ((idtr_ptr.addr) + idtr_ptr.limit))
+	{
+		/**
+		uint64_t  offset_1:16;     bits 00-15 of the isr offset
+		uint64_t selector : 16;    isr segment selector
+		uint64_t ist : 3;		   stack table: only 64 bits
+		uint64_t zero_1 : 5;	   must be 0
+		uint64_t type : 4;		   interrupt/trap gate
+		uint64_t zero_2 : 1;	   must be zero
+		uint64_t dpl : 2;		   privilege level
+		uint64_t p : 1;			   present flag
+		uint64_t offset_2 : 16;    bits 16-31 of the isr offset
+		**/
+		debug("%d offset %d, ", i, idt_ptr->offset_1 | idt_ptr->offset_2 << 16);
+		debug("selector %d, ", idt_ptr->selector);
+		debug("ist %d, ", idt_ptr->ist);
+		debug("type %d, ", idt_ptr->type);
+		debug("dpl %d, ", idt_ptr->dpl);
+		debug("p %d, ", idt_ptr->p);
+		debug("\n");
+		idt_ptr++;
+		i++;
+	}
+}
 
 void bp_handler() {
-   // TODO
+	asm volatile ("pusha");
+
+	uint32_t val;
+	asm volatile ("mov 4(%%ebp), %0":"=r"(val));
+	debug("ebp-4: %x\n", val);
+	debug("BP Handler !\n");
+
+	asm volatile ("popa\n\t"
+		          "leave\n\t"
+				  "iret");
 }
 
 void bp_trigger() {
-	// TODO
+	asm volatile ("int3");
+	debug("On a rendu la main c'est dingue !!!\n");
 }
 
 void tp() {
 	// TODO print idtr
+	idt_reg_t idtr;
+	get_idtr(idtr);
+	print_idt_content(idtr);
+
+	int_desc_t bp_desc;
+	raw32_t offset = {.raw = (uint32_t)bp_handler};
+	bp_desc.offset_1 = offset.wlow;
+	bp_desc.offset_2 = offset.whigh;
+	bp_desc.zero_1 = 0;
+	bp_desc.zero_2 = 0;
+	bp_desc.ist = 0;
+	bp_desc.dpl = 0;
+	bp_desc.p = 1;
+	bp_desc.selector = 8;
+	bp_desc.type = 14;
+
+	idtr.desc[3] = bp_desc;
 
 	// TODO call bp_trigger
-   //bp_trigger();
+	bp_trigger();
 }
--- tp3/tp.c
+++ tp3/tp.c
@@ -1,10 +1,112 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <segmem.h>
 
 void userland() {
    asm volatile ("mov %eax, %cr0");
 }
 
 void tp() {
-   // TODO
+   // ========
+   // GDTR
+   // ========
+   seg_desc_t descs[6];
+
+   // desc0 = NULL
+   descs[0].raw = 0;
+
+   // Code, 32 bits RX, flat, ring 0
+   descs[1].limit_1 = 0xFFFF;
+   descs[1].base_1 = 0x0000;
+   descs[1].base_2 = 0x00;
+   descs[1].type = SEG_DESC_CODE_XR;
+   descs[1].s = 1;
+   descs[1].dpl = 0;
+   descs[1].p = 1;
+   descs[1].limit_2 = 0xF;
+   descs[1].avl = 0;
+   descs[1].l = 0;
+   descs[1].d = 1;
+   descs[1].g = 1;
+   descs[1].base_3 = 0x00;
+   // seg_sel_t seg1 = {.index = 1, .rpl = 0, .ti = 0};
+
+   // Données, 32 bits RW, ring 0
+   descs[2].limit_1 = 0xFFFF;
+   descs[2].base_1 = 0x0000;
+   descs[2].base_2 = 0x00;
+   descs[2].type = SEG_DESC_DATA_RW;
+   descs[2].s = 1;
+   descs[2].dpl = 0;
+   descs[2].p = 1;
+   descs[2].limit_2 = 0xF;
+   descs[2].avl = 0;
+   descs[2].l = 0;
+   descs[2].d = 1;
+   descs[2].g = 1;
+   descs[2].base_3 = 0x00;
+   seg_sel_t seg2 = {.index = 2, .rpl = 0, .ti = 0};
+
+   // Données, 32 bits RW, base 0x600000, limite 32 octets, ring 0
+   descs[3].limit_1 = 0x20;
+   descs[3].base_1 = 0x0000;
+   descs[3].base_2 = 0x60;
+   descs[3].type = SEG_DESC_DATA_RW;
+   descs[3].s = 1;
+   descs[3].dpl = 0;
+   descs[3].p = 1;
+   descs[3].limit_2 = 0x0;
+   descs[3].avl = 0;
+   descs[3].l = 0;
+   descs[3].d = 1;
+   descs[3].g = 0;
+   descs[3].base_3 = 0x00;
+   seg_sel_t seg3 = {.index = 3, .rpl = 0, .ti = 0};
+
+   // Code, 32 bits RX, ring 3, flat
+   descs[4].limit_1 = 0xFFFF;
+   descs[4].base_1 = 0x0000;
+   descs[4].base_2 = 0x00;
+   descs[4].type = SEG_DESC_CODE_XR;
+   descs[4].s = 1;
+   descs[4].dpl = 3;
+   descs[4].p = 1;
+   descs[4].limit_2 = 0xF;
+   descs[4].avl = 0;
+   descs[4].l = 0;
+   descs[4].d = 1;
+   descs[4].g = 1;
+   descs[4].base_3 = 0x00;
+   seg_sel_t seg4 = {.index = 4, .rpl = 3, .ti = 0};
+
+   // Data, 32 bits RW, ring 3, flat
+   descs[5].limit_1 = 0xFFFF;
+   descs[5].base_1 = 0x0000;
+   descs[5].base_2 = 0x00;
+   descs[5].type = SEG_DESC_DATA_RW;
+   descs[5].s = 1;
+   descs[5].dpl = 3;
+   descs[5].p = 1;
+   descs[5].limit_2 = 0xF;
+   descs[5].avl = 0;
+   descs[5].l = 0;
+   descs[5].d = 1;
+   descs[5].g = 1;
+   descs[5].base_3 = 0x00;
+   seg_sel_t seg5 = {.index = 5, .rpl = 3, .ti = 0};
+
+   gdt_reg_t new_gdtr;
+   new_gdtr.desc = descs;
+   new_gdtr.limit = sizeof(descs);
+
+   set_gdtr(new_gdtr);
+
+   set_ds(seg5);
+   // set_ss(seg5);
+   set_es(seg3);
+   set_fs(seg5);
+   set_gs(seg5);
+    
+   fptr32_t farjump = {.offset = (uint32_t)&userland, .segment = seg4.raw};
+   farjump(farjump);
 }
